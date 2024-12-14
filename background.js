20bd7b1f45dabd02303ca0875eb4d09eabee67de
Sameer Shrestha <sameershrestha04@gmail.com>
2024-12-14 18:17:24 +0545

    reddit ad blocker

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log('background.js loaded');
+function hideads(){
+    const ads = document.querySelectorAll("shreddit-ad-post");
+    ads.forEach((ad) =>{
+        ad.style.display = "none";
+    } );
+}
+const resizeObserver = new ResizeObserver(() => {
+    hideads();
+});
+resizeObserver.observe(document.body);
